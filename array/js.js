d98154e413c0ee84e62881b792ebb3e57fb480d9
Md. Bariul Munshi <bariulsoft@gmail.com>
2023-02-01 23:41:44 +0600

    third commit

---
+++ array/js.js
@@ -0,0 +1,51 @@
+// Declare Array, array length
+var friendsage = [23, 22, 20, 20];
+var murichanacurFee = [20, 25, 20, 30];
+var nakies = ["Hila", "Lila", "Mila", "Tila"];
+var oddNumbers = ["1", "3", "5", "7", "9"];
+var vowels = ["a", "e", "i", "o", "u"];
+// console.log(friendsage);
+// console.log(oddNumbers);
+// console.log(nakies);
+// console.log(vowels.length);
+
+// array index position findout
+var bookPackages = ["Hablu", "bolod", "hjem", "ej"];
+// var bolodindex = bookPackages.indexOf("bolod");
+// console.log(bookPackages.indexOf("bolod"));
+
+var numbers = [12, 21, 22, 44, 44];
+// console.log(numbers.indexOf(22));
+
+// get value by index numbers
+// console.log(numbers[0]);
+
+// set value by  index numbers
+numbers[1] = 100;
+// console.log(numbers);
+
+// push element to array
+var nakies = ["Hila", "Lila", "Mila", "Tila"];
+nakies.push("Pori-Moni");
+nakies.push("Katrina");
+// console.log(nakies);
+
+var friendsage = [23, 22, 20, 20];
+friendsage.push(12);
+// console.log(friendsage);
+
+// remove/pop/insert an element from an array
+var friendsage = [23, 22, 20, 20];
+friendsage.pop();
+friendsage.pop();
+lastItem = friendsage.pop();
+// console.log(friendsage);
+// console.log(lastItem);
+
+// in javascript Remove first item from an Array and add array element in the beginning
+var friends = ["Anik", "Naim", "Plabon", "Naimur", "Rakib", "Minhaz"];
+friends.shift();
+// console.log(friends);
+var friends = ["Anik", "Naim", "Plabon", "Naimur", "Rakib", "Minhaz"];
+friends.unshift("Numan");
+// console.log(friends);
---
+++ array/stepbystep.js
@@ -0,0 +1,48 @@
+/* What's Array?
+An array in JavaScript is a type of global object used to store multiple values in a single variable.*/
+//Example:
+var myArray = ["John", "Paul", "Ringo", "George"];
+
+
+//1.print your 5 friends age using Array
+var firendsAge =[20,23,25,22,21,24];
+console.log(firendsAge);
+
+//2.print your 5 friends name using Array
+var friendsName =['Hasibul','Rokibul','Shawon','Anik','Plabon'];
+console.log(friendsName);
+
+//3.show Vowels and count the number of it using Array
+var vowels =['a','e','i','o','u'];
+console.log(vowels);
+console.log(vowels.length);
+
+//4. ['a','e','i','o','u']  findout the index position of o and findout value of 0 index.
+var vowels =['a','e','i','o','u'];
+console.log(vowels.indexOf('o')); //or var oIndex=vowels.indexof('o');
+console.log(vowels[0]);
+
+//5.[20,23,25,22,21,24] updates 0 index value and set it 21.
+var value =[20,23,25,22,21,24];
+value[0]=21;
+console.log(value);
+
+//6. ["Hila", "Lila", "Mila", "Tila"] in this Array remove 'Mila','Tila' and entery new name 'Bariul' from the Array.
+var nameCollection=["Hila", "Lila", "Mila", "Tila"];
+nameCollection.pop()
+nameCollection.pop()
+nameCollection.push('Bariul');
+console.log(nameCollection);
+
+//7.In javascript Remove first item from an Array and add array element in the beginning
+var friends = ["Anik", "Naim", "Plabon", "Naimur", "Rakib", "Minhaz"];
+friends.shift(); //Remove first item
+// console.log(friends);
+var friends = ["Anik", "Naim", "Plabon", "Naimur", "Rakib", "Minhaz"];
+friends.unshift("Numan"); //add array element in the beginning
+//console.log(friends);
+
+
+
+
+
