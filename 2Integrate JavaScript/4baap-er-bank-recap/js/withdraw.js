af07cec114597d9e9d8492d52a50f6ffe2a9a225
Md. Bariul Munshi <bariulsoft@gmail.com>
2023-03-19 08:15:50 +0600

    added new bank calculation using function

---
+++ 2Integrate JavaScript/4baap-er-bank-recap/js/withdraw.js
@@ -0,0 +1,14 @@
+//for btn
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+  //input
+  const newWithdrawAmount=getInputFieldValueById('withdraw-field');
+  //output
+  const previousWithdrawTotal=getOutputValueById('withdraw-total')
+  const newWithdrawTotal=previousWithdrawTotal+newWithdrawAmount;
+  setTextOutputValueByID('withdraw-total',newWithdrawTotal);
+
+  //output for balance
+  const previousBalanceTotal=getOutputValueById('balance-total');
+  const newBalanceTotal=previousBalanceTotal-newWithdrawAmount;
+  setTextOutputValueByID('balance-total',newBalanceTotal);
+})
